1765f1812982331ee82e54d2713077546f65edbf
ravlick <svtrojan16@gmail.com>
2021-10-05 13:01:25 +0300

    reload project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+    $('.header__burger').on('click', function () {
+        $('.header__top-navbar').toggleClass('menu--active');
+
+    });
+
+    // $('.menu__btn').on('click', function () {
+    //     $('.menu').removeClass('menu--active');
+    // });
+    // $('.menu__item').on('click', function () {
+    //     $('.menu').removeClass('menu--active');
+    // });
+});
